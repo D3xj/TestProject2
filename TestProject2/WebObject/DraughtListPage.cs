4316d9a28d64a38836a038078a699104fa54858f
Alexey Deynichenko <DexJL@yandex.ru>
2022-02-03 15:13:51 +0300

    Fixed _draughtListLbl

--- TestProject2/WebObject/DraughtListPage.cs
+++ TestProject2/WebObject/DraughtListPage.cs
@@ -9,7 +9,7 @@ namespace TestProject2.WebObject
 {
     public class DraughtListPage : BasePage
     {
-        private readonly static By _draughtListLbl = By.XPath("//span[@class = 'rui-Button-content']"); //Исправить
+        private readonly static By _draughtListLbl = By.XPath("//span[@class = 'rui-Button-content']");
 
         private readonly BaseElement _toBeDeletedMessage = new BaseElement(By.XPath("//span[@class='Checkbox-root-vD']"));
         private readonly BaseElement _toDelete = new BaseElement(By.XPath("//span[@class = 'ButtonWithIcon-icon-1Q ButtonWithIcon-iconLeft-2c ButtonWithIcon-iconBig-2w']"));
