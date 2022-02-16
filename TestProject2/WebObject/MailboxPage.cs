632a3f14cb0da10958f9bbdb1282f2b6e1eab070
Alexey Deynichenko <DexJL@yandex.ru>
2022-02-16 15:00:09 +0300

    Added CheckReceived method

--- TestProject2/WebObject/MailboxPage.cs
+++ TestProject2/WebObject/MailboxPage.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,12 @@ namespace TestProject2.WebObject
 {
     public class MailboxPage : BasePage
     {
-        private readonly static By _mailboxLbl = By.XPath("//span[@class = 'ButtonWithIcon-icon-1Q ButtonWithIcon-iconLeft-2c ButtonWithIcon-iconBig-2w']");
+        private readonly static By _mailboxLbl = By.XPath("//div[text()='Поиск по отправителю']");
 
         private readonly BaseElement _writeEmailButton = new BaseElement(By.XPath
             ("//span[@class = 'rui-Button-content']"));
-        private readonly BaseElement _draftList = new BaseElement(By.XPath("//span[text() = 'Черновики']"));
+        private readonly BaseElement _draftList = new BaseElement(By.XPath("//div[@class='FoldersItem-root-3P']//span[text() = 'Черновики']"));
+        private readonly BaseElement _sentPage = new BaseElement(By.XPath("//div[@class='FoldersItem-root-3P']//span[text() = 'Отправленные']"));
 
         public MailboxPage() : base(_mailboxLbl, "Mailbox Page"){}
 
@@ -22,9 +24,21 @@ namespace TestProject2.WebObject
             _writeEmailButton.Click();
         }
 
+        public void CheckReceived(string subject)
+        {
+            BaseElement messageSubject = new BaseElement(By.XPath("//span[@class = 'ListItem-subject-2M'][text() = '" + subject + "']"));
+            messageSubject.WaitForIsVisible();
+            Assert.IsTrue(messageSubject.Displayed);
+        }
+
         public void GoToDraughtList()
         {
             _draftList.Click();
+        }
+
+        public void GoToSentPage()
+        {
+            _sentPage.Click();
         }
     }
 }
