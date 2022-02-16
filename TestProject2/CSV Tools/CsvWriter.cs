f77ef1c1f7870b33af6023eb174eece7b77a58cd
Alexey Deynichenko <DexJL@yandex.ru>
2022-02-16 14:50:44 +0300

    Added Csv Tools

---
+++ TestProject2/CSV Tools/CsvWriter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+
+namespace TestProject2.CSV_Tools
+{
+    public class CSVWriter
+    {
+        public void CsvWriter(string email, string subj, string message, int num)
+        {
+            var notes = new List<Foo>
+            {
+            new Foo{toWhom = email, Subject = subj, Message = message, Number = num }
+            };
+            using (var writer = new StreamWriter(@"C:\Users\pc\Desktop\TestProject2\TestProject2\Resources\CheckIncoming.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(notes);
+            }
+        }
+    }
+}
---
+++ TestProject2/CSV Tools/Foo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject2.CSV_Tools
+{
+    public class Foo
+    {
+        public string toWhom { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public int Number { get; set; }
+    }
+}
